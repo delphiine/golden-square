c381f3155418bae346429a6b2bdc6e5bbace6c66
delphine <delphine.mubiligi@gmail.com>
2022-06-14 14:13:03 +0100

    testing adding 1 to counter

--- spec/counter_spec.rb
+++ spec/counter_spec.rb
@@ -5,4 +5,9 @@ RSpec.describe 'returns report of counter' do
         new_counter = Counter.new
         expect(new_counter).to be_a(Counter)
     end
+    it 'test adding 1' do
+        new_counter = Counter.new
+        new_counter.add(1)
+        expect(new_counter.report).to eq "Counted to 1 so far."
+    end
 end
