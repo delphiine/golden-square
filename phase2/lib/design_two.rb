567087c2a108411bf464b86067ec77e2d6cb207a
delphine <delphine.mubiligi@gmail.com>
2022-06-16 20:32:57 +0100

    added grammer checker method and rspec test

--- phase2/lib/design_two.rb
+++ phase2/lib/design_two.rb
@@ -1,9 +1,12 @@
-=begin
 def grammar_checker(to_check)
+    fail "Not a sentence." if to_check.empty?
+
     alphabet = ('A'..'Z').to_a 
     if alphabet.include?(to_check[0]) && to_check[-1] == "."
         return "Your grammar is correct"
-    elsif 
-
+    elsif !alphabet.include?(to_check[0]) && to_check[-1] == "."
+        return "Remember to capitalize your sentence"
+    elsif alphabet.include?(to_check[0]) && to_check[-1] != "."
+        return "Please review your punctuation"
+   end
 end
-=end
--- phase2/spec/design_two_spec.rb
+++ phase2/spec/design_two_spec.rb
@@ -1,9 +1,16 @@
-=begin
 require 'design_two'
 
 RSpec.describe 'grammar_checker' do
+    it "Fails when method is given an empty string" do
+        expect {grammar_checker("")}.to raise_error "Not a sentence."
+    end
     it "Returns 'Your grammar is correct' if a string starts with capital letter and ends with full stop" do
         expect(grammar_checker("Hello, world.")).to eq "Your grammar is correct"
     end
+    it "Reminds the user to capitalize their sentence" do
+        expect(grammar_checker("hello, world.")).to eq "Remember to capitalize your sentence"
+    end
+    it "Reminds the user to add a full stop at the end of their sentence" do
+        expect(grammar_checker("Hello, world")).to eq "Please review your punctuation"
+    end
 end
-=end
