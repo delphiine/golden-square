54a4e8dd7525cd0c9831de6050069f167629a1b4
delphine <delphine.mubiligi@gmail.com>
2022-06-17 10:16:03 +0100

    added array of punktuation marks

--- phase2/lib/design_two.rb
+++ phase2/lib/design_two.rb
@@ -1,12 +1,14 @@
 def grammar_checker(to_check)
     fail "Not a sentence." if to_check.empty?
 
-    alphabet = ('A'..'Z').to_a 
-    if alphabet.include?(to_check[0]) && to_check[-1] == "."
+    alphabet = ('A'..'Z').to_a
+    punctuation = ["!", ".", "?"]
+     
+    if alphabet.include?(to_check[0]) && punctuation.include?(to_check[-1])
         return "Your grammar is correct"
-    elsif !alphabet.include?(to_check[0]) && to_check[-1] == "."
+    elsif !alphabet.include?(to_check[0]) && punctuation.include?(to_check[-1])
         return "Remember to capitalize your sentence"
-    elsif alphabet.include?(to_check[0]) && to_check[-1] != "."
+    elsif alphabet.include?(to_check[0]) && !punctuation.include?(to_check[-1])
         return "Please review your punctuation"
    end
 end
