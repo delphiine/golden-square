d9a2a3ce30abf329275ff405e2856c3db0bdf43e
delphine <delphine.mubiligi@gmail.com>
2022-06-23 17:14:20 +0100

    updated unit tests

--- Phase3/mocking_bites/spec/task_list_spec.rb
+++ Phase3/mocking_bites/spec/task_list_spec.rb
@@ -12,4 +12,41 @@ RSpec.describe TaskList do
   end
 
   # Unit test `#all` and `#all_complete?` behaviour
+  context "returns add task list items" do
+    it "Returns one task" do
+      task_list = TaskList.new
+      task = double(:fake_task)
+      task_list.add(task)
+      expect(task_list.all).to eq [task]
+    end
+
+    it "Returns multiple tasks" do
+      task_list = TaskList.new
+      task_1 = double(:fake_task)
+      task_2 = double(:fake_task)
+      task_list.add(task_1)
+      task_list.add(task_2)
+      expect(task_list.all).to eq [task_1, task_2]
+    end
+
+  end 
+
+  it "Returns true when tasks have been marked as completed" do
+    task_list = TaskList.new
+    task = double(:fake_task, mark_complete: nil, complete?: true)
+    task_list.add(task)
+    task.mark_complete
+    expect(task_list.all_complete?).to eq true
+  end
+
+  it "Returns false if at least one task is yet to be marked 'complete'" do
+    task_list = TaskList.new
+    task_1 = double(:fake_task, mark_complete: nil, complete?: true)
+    task_2 = double(:fake_task, mark_complete: nil, complete?: false)
+    task_list.add(task_1)
+    task_list.add(task_2)
+    task_1.mark_complete
+    task_2.mark_complete
+    expect(task_list.all_complete?).to eq false
+  end
 end
